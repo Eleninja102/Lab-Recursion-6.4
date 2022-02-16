57ffee7a277d0cc5723af3fabc90239f829014bf
Coleton Watt <electricninja102@gmail.com>
2022-02-16 12:23:09 -0700

    Initial Commit

---
+++ Lab: Recursion 6.4/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Lab: Recursion 6.4
+//
+//  Created by Coleton Watt on 2/16/22.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
