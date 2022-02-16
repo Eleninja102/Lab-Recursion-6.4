4a2090c70557a8a27283d420b3b24a6ff424e218
Coleton Watt <electricninja102@gmail.com>
2022-02-16 13:20:56 -0700

    even more test cases

--- Lab: Recursion 6.4/SubsequenceTester.cpp
+++ Lab: Recursion 6.4/SubsequenceTester.cpp
@@ -20,8 +20,17 @@ int main()
     checkCase("Case 5: Code", "sel", "string testCaseName, string sub, string sentence, bool correctResponse", true);
     checkCase("Case 6: multiple", "bad", "bbbbbbbbbbbbddd", false);
     checkCase("Case 7: multiple good", "bad", "bbbbbbbbbbbbdaaaaadd", true);
-    checkCase("Case 7: more code", "iflwirrmeaiintargcntodhstt0;", "bool isPalindrome(string word, int first, int last ){if(word[first] != word[last]){return false;}else if(first == last){return true;}else{bool x = isPalindrome(word, ++first, --last);return x;}}//Facilitate Recursive method callbool isPalindrome(string word){return isPalindrome(word, 0, word.length()-1);}int main(int argc, const char * argv[]) {// insert code here...std::cout << ;std::cout << go(4) << endl;rain(4);return 0;}", true);
+    checkCase("Case 8: more code", "iflwirrmeaiintargcntodhstt0;", "bool isPalindrome(string word, int first, int last ){if(word[first] != word[last]){return false;}else if(first == last){return true;}else{bool x = isPalindrome(word, ++first, --last);return x;}}//Facilitate Recursive method callbool isPalindrome(string word){return isPalindrome(word, 0, word.length()-1);}int main(int argc, const char * argv[]) {// insert code here...std::cout << ;std::cout << go(4) << endl;rain(4);return 0;}", true);
+    checkCase("Case 9: more code+", "iflwirrmeaiintargcmdbdre0ntodhstt0zz;", "bool isPalindrome(string word, int first, int last ){if(word[first] != word[last]){return false;}else if(first == last){return true;}else{bool x = isPalindrome(word, ++first, --last);return x;}}//Facilitate Recursive method callbool isPalindrome(string word){return isPalindrome(word, 0, word.length()-1);}int main(int argc, const char * argv[]) {// insert code here...std::cout << ;std::cout << go(4) << endl;rain(4);return 0;}", false);
+    checkCase("Case 10: more code++", "iflwirrmeaiintargcntodhstt0zz;", "bool isPalindrome(string word, int first, int last ){if(word[first] != word[last]){return false;}else if(first == last){return true;}else{bool x = isPalindrome(word, ++first, --last);return x;}}//Facilitate Recursive method callbool isPalindrome(string word){return isPalindrome(word, 0, word.length()-1);}int main(int argc, const char * argv[]) {// insert code here...std::cout << ;std::cout << go(4) << endl;rain(4);return 0;}", false);
+    checkCase("Case 11: more+++ code", "isl;", "bool isPalindrome(string word, int first, int last ){if(word[first] != word[last]){return false;}else if(first == last){return true;}else{bool x = isPalindrome(word, ++first, --last);return x;}}//Facilitate Recursive method callbool isPalindrome(string word){return isPalindrome(word, 0, word.length()-1);}int main(int argc, const char * argv[]) {// insert code here...std::cout << ;std::cout << go(4) << endl;rain(4);return 0;}", true);
+    checkCase("Case 12: more++++ code", "if[lwirm{eai}intarg,cntodhstt0;", "bool isPalindrome(string word, int first, int last ){if(word[first] != word[last]){return false;}else if(first == last){return true;}else{bool x = isPalindrome(word, ++first, --last);return x;}}//Facilitate Recursive method callbool isPalindrome(string word){return isPalindrome(word, 0, word.length()-1);}int main(int argc, const char * argv[]) {// insert code here...std::cout << ;std::cout << go(4) << endl;rain(4);return 0;}", true);
+    checkCase("Case 13: multiple what", "aaaaaaaaaaa", "aaaadaaaaaaaaaaaaaaaaaa", true);
+    checkCase("Case 14: multiple what", "aaaadaaaaaaa", "aaaadaaaaaaaaaaaaaaaaaa", true);
 
+
+
+    
 
     
 
